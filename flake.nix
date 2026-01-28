b37f7780a38026b0fd6ecbf9e96c86fe1693118b
Gabriel Fontes <hi@m7.rs>
2026-01-28 16:09:34 -0300

    go back to an older version of matugen
    The new one absolutely changed the format

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Personal wallpapers and colorschemes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     systems.url = "github:nix-systems/default-linux";
   };
 
